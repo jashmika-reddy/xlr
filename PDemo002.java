dda388d8829300692dd492848a74253f55ed88a5
jashmika reddy <jashmikareddyg@gmail.com>
2025-02-03 10:54:32 +0530

    greatest between 2 numbers practice

---
+++ PDemo002.java
@@ -0,0 +1,12 @@
+public class PDemo002 {
+    public static void main(String[] args){
+        int a=20;
+        int b=50;
+        if(a>b){
+            System.out.println(a+ " is greater");
+        }
+        else{
+            System.out.println(b+ " is greater");
+        }
+    }
+}
