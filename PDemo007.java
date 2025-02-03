a21f3b32ab2a84c01985d7733458dc70e6bd5a5d
jashmika reddy <jashmikareddyg@gmail.com>
2025-02-03 10:59:52 +0530

    scanner class practice

---
+++ PDemo007.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class PDemo007 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int number = scanner.nextInt();
+        System.out.print("Enter a string: ");
+        String a = scanner.nextLine();
+    }
+}
