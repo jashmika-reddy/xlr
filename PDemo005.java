2f1f943fe8bb635008690017bc0a0b25f3512e31
jashmika reddy <jashmikareddyg@gmail.com>
2025-02-03 10:58:53 +0530

    while loop practice

---
+++ PDemo005.java
@@ -0,0 +1,10 @@
+public class PDemo005 {
+    public static void main(String[] args) {
+        String username = "Jashmika";
+        String password = "jashmika123";
+        while((username=="Jashmika") && (password=="jashmika123")){
+            System.out.println("hi");
+            break;
+        }
+    }
+}
