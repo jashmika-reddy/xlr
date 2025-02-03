22371ec0e3f0fc1649f122843f9d9dfff5cee9da
jashmika reddy <jashmikareddyg@gmail.com>
2025-02-03 10:57:44 +0530

    printing 1-100 numbers practice

---
+++ PDemo004.java
@@ -0,0 +1,8 @@
+public class PDemo004 {
+    public static void main(String[] args) {
+        System.out.println("printing 1 to 100 numbers");
+        for(int i=0;i<=100;i++){
+            System.out.println(i);
+        }
+    }
+}
