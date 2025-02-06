39125de3d60e3fc85897e61995cefa71816286cb
jashmika-reddy <jashmikareddyg@gmail.com>
2025-02-06 12:16:21 +0530

    printing array elements

---
+++ Demo065.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+public class Demo065 {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50}; 
+        System.out.println("Array elements: " + Arrays.toString(arr));
+    }
+}
+
