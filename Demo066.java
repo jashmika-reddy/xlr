eb0f7ff14045678142b895d84df0aa21ea6e85b0
jashmika-reddy <jashmikareddyg@gmail.com>
2025-02-06 12:33:36 +0530

    printing index values of an array

---
+++ Demo066.java
@@ -0,0 +1,12 @@
+public class Demo066 {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        System.out.println("Index | Value");
+        System.out.println("---------------");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("  " + i + "   |  " + numbers[i]);
+        }
+    }
+}
+
+
